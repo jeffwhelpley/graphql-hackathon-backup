826f134e6e0e379d52282a9e97ef03f264328a10
Jeff Whelpley <jeff@gethuman.com>
2017-02-10 14:16:48 -0500

    merging in changes

--- packages/graphql-server/__build__/src/types.d.ts
+++
@@ -1,4 +0,0 @@
-import { GraphQLObjectType, GraphQLInputObjectType } from 'graphql';
-export declare const authorType: GraphQLObjectType;
-export declare const bookType: GraphQLObjectType;
-export declare const bookInputType: GraphQLInputObjectType;
